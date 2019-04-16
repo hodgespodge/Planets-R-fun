c8403f3d05ab39cc7b04b3ab9f9436dbc8fdb787
John Muse <john.r.muse@gmail.com>
2019-04-15 22:56:38 -0400

    Working on organizing or whether to organize some classes  related to vectors and locations.

--- src/Location.java
+++ src/Location.java
@@ -1,11 +1,26 @@
-public class Location extends Point {
+public class Location {
+
+    private double x_1, x_2;
 
     public Location(double x, double y) {
-        super(x, y);
+        this.x_1 = x;
+        this.x_2 = y;
+    }
+
+    public double get_x() {
+        return this.x_1;
+    }
+
+    public double get_y() {
+        return this.x_2;
     }
 
     // need a method for determining the distance from this location to another.
     public double get_distance_to_location(Location l) {
-        
+        Location n = new Location(this.get_x() - l.get_x(), this.get_y() - l.get_y());
+        return Math.sqrt(n.get_x() * n.get_x() + n.get_y() * n.get_y());
     }
+
+
+    // static functions
 }
--- src/Vector.java
+++ src/Vector.java
@@ -1,13 +1,22 @@
-public class Vector extends Point {
-    private Point p;
+public class Vector {
+    private double x_1, x_2;
     private Vector normalized;
     private double length;
 
 
     public Vector(double x_1, double x_2) {
-        super(x_1, x_2);
+        this.x_1 = x_1;
+        this.x_2 = x_2;
         this.length = norm(this);
         this.normalized = normalize(this);
+    }
+
+    public double get_x_1() {
+        return this.x_1;
+    }
+
+    public double get_x_2() {
+        return this.x_2;
     }
 
     public double get_length() {
