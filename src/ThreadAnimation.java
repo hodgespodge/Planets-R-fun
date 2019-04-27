76432165f791e6e1e8052e0cbd56c88e164facf0
Jones42 <47765738+Jones42@users.noreply.github.com>
2019-04-27 13:09:32 -0400

    Update ThreadAnimation.java

--- src/ThreadAnimation.java
+++ src/ThreadAnimation.java
@@ -10,7 +10,22 @@ public class ThreadAnimation extends JFrame {
 
     private void initUI() {
 
-        add(new Board());
+        JPanel board = new Board();
+        add(board);
+
+        JSplitPane splitPane = new JSplitPane();
+        JPanel bottomPanel = new BottomPanel();
+
+        setPreferredSize(new Dimension(F_WIDTH, F_HEIGHT));
+        getContentPane().setLayout(new GridLayout());
+        getContentPane().add(splitPane);
+
+        splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+        splitPane.setDividerLocation(F_DIVIDE);
+        splitPane.setTopComponent(board);
+        splitPane.setBottomComponent(bottomPanel);
+
+        bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.X_AXIS));
 
         setResizable(false);
         pack();
