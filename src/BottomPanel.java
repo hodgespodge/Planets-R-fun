51c56d93f50177866492a9a332fae91596f12a04
Jones42 <47765738+Jones42@users.noreply.github.com>
2019-04-27 13:11:20 -0400

    Create BottomPanel.java

---
+++ src/BottomPanel.java
@@ -0,0 +1,22 @@
+    
+import javax.swing.*;
+import java.awt.*;
+
+public class BottomPanel extends JPanel{
+
+    BottomPanel() {
+        run();
+    }
+
+    private void run() {
+        setBackground(Color.LIGHT_GRAY);
+
+        JLabel label = new JLabel("PLAAAAAANETS ARE FUUUUUUUUUN");
+
+        while(true) {
+
+
+            add(label);
+        }
+    }
+}
