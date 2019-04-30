e9bda3f6b8cebb1decbf3c7c56388a0d5af8af54
hodgespodge <shodges@unca.edu>
2019-04-30 11:16:02 -0400

    Added in some example code in the Thread Animation main method. These functions should generally be implemented as buttons in some other UI classes

--- src/ThreadAnimation.java
+++ src/ThreadAnimation.java
@@ -1,10 +1,13 @@
 import java.awt.*;
 import javax.swing.*;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class ThreadAnimation extends JFrame {
 
-    private final int F_WIDTH = 950;
-    private final int F_HEIGHT = 600;
+    private final int F_WIDTH = 1050;
+    private final int F_HEIGHT = 700;
     private final int F_DIVIDE = 400;
 
     public ThreadAnimation() {
@@ -45,5 +48,29 @@ public class ThreadAnimation extends JFrame {
             JFrame ex = new ThreadAnimation();
             ex.setVisible(true);
         });
+
+
+        /**
+         *
+         * FOR TESTING FEATURES
+         *
+         * ALL OF THIS MAY BE DONE IN ANY OTHER CLASS AS IT ALL OPERATES ON THE UNIVERSE.GETINSTANCE
+         *
+         */
+
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+        while(true){
+            System.out.println("Planet size =  ");
+            int radius = scan.nextInt();
+            Universe.getInstance().addParticle(new Planet(100000, new Location(rand.nextInt(600), rand.nextInt(600)), new Vector(0, 0), radius,Color.YELLOW));
+
+            System.out.println("Set Gravity?");
+            if(!scan.next().equalsIgnoreCase("No")){
+                System.out.println("Gravity = ");
+                Universe.getInstance().setGravityScale(scan.nextDouble());
+
+            }else Universe.getInstance().setGravityScale(Universe.getInstance().getGravitationlConstant());
+        }
     }
 }
